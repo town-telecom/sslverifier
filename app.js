90709e0f4a7e3578c2f096aa60f2f981b12d1a6a
Guillermo X. Aiken <aikengx@gmail.com>
2019-04-18 15:53:35 -0400

    refactored to use map method instead of for loop

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const extension = ".com";
 const domains = require ('./data.js');
 const request = require('request');
+const verifyDomains = (arr) => {
 
-for (let i = 0; i < domains.length; i++) {
+  arr.map(domain => {
 
-  let url = "https://www." + domains[i] + extension;
-  request(url, (err, res) => {
-    if (err) {
-      console.log(url + " => Error!", err);
-    } else {
-      console.log(url + " => OK!");
-    }
+    let url = "https://www." + domain + extension;
+    request(url, (err, res) => {
+      if (err) {
+        console.log(url + " => Error!", err);
+      } else {
+        console.log(url + " => OK!");
+      }
     });
 
+  });
+
 };
+
+verifyDomains(domains);
+
--- data.js
+++ data.js
@@ -1,5 +1,3 @@
-const domainList = [
-
-];
+const domainList = [];
 
 module.exports = domainList;
